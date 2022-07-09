cf9141a0f584e661d8378ba6bf86faeb52cf76c7
Christian Staudt <mail@clstaudt.me>
2022-07-09 20:16:54 +0200

    WIP : refactoring discussion

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ import time
 import requests
 import argparse
 from getSimilarity import calc_score
-from getID import getID
+from get_matching_file_name import get_matching_file_name
 
 flask_app = Flask(__name__)
 
@@ -28,22 +28,26 @@ def home():
 @flask_app.route("/search", methods=["GET", "POST"])
 def search():
     # Param 1 $sentence$. Retrieve the search input from the user text-ins.
-    sentence = request.form.get("inputText")
-    target = request.args.get("question")
-    target = escape(target)
+    query_text = request.form.get("inputText")
 
     # Param 2 $fileName$. Get the selected file_name.
     # FIXME: read all data from the /data folder
-    save_path = "./notebooks/transcription/data_samples.csv"
+    search_index_path = "./notebooks/transcription/data_samples.csv"
+    # TODO: data schema for knowledge base
+    #
+    # TODO: data schema for search response
     # fileName = './notebooks/transcription/data_mp3/' + getID(save_path, sentence)
-    fileName = "./static/data/" + getID(save_path, sentence)
+    fileName = "./static/data/" + get_matching_file_name(
+        search_index_path, 
+        query_text
+    )
     # Parm 3 & 4 $startTime$ $endTime$ Here are the time we need to trim, which they will send back to the client side.
     start_time = 0
     end_time = 100
 
     return render_template(
         "test.html",
-        sentence=f"{sentence}",
+        sentence=f"{query_text}",
         fileName=f"{fileName}",
         startTime=f"{start_time}",
         endTime=f"{end_time}",
