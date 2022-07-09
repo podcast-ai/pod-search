9f6b2dbe20662a1ad62151f8ccc4f2a505496ca4
Xiao Zhang <58770171+xiao11lam@users.noreply.github.com>
2022-07-09 02:36:27 -0700

    Update the search engine

---
+++ getID.py
@@ -0,0 +1,16 @@
+from getSimilarity import calc_score
+
+
+def getID(save_path, sentence):
+    with open(save_path, "r") as file:
+        # Generate the dict in {audioID: Trans}
+        d = dict([line.strip().split(',') for line in file])
+        # Generate the dict in {ID: Score}
+        for id in d:
+            d[id] = calc_score(sentence, d[id])
+        # Sort the {ID: Score} and pick the highest value
+        sort_orders = sorted(d.items(), key=lambda x: x[1], reverse=True)
+        # retreive the audioID by the highest similarity value
+        ID = sort_orders[0][0]
+        return ID
+
