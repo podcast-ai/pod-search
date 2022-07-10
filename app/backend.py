e95248c5a09c0f3b2420d701818283671117d7b6
adrian-zuur <adrian.zuur@globant.com>
2022-07-09 22:16:24 -0500

    changed mocked backend

--- app/backend.py
+++ app/backend.py
@@ -6,7 +6,7 @@ def query(query_text):
             'id': str(x),
             'fileName': f"./static/data/speech_samples/sample-00000{x}.mp3",
             'start_proportion': x/6,
-            'podcast_title': f'Podcast title {x}',
+            'podcast_title': f'Podcast title, episode {x}',
             'podcast_url': f'https://www.podcast{x}.com', # keep the http(s)!
             'podcast_info': f'This is info to print about the chunk or podcast. Chunk {x} is really interesting...' 
         } 
