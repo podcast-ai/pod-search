eae0b3f14c3ed885b654e26a43cae3d549736e9a
Christian Staudt <mail@clstaudt.me>
2022-07-10 13:48:34 +0200

    index worker

--- engine/worker.py
+++ engine/worker.py
@@ -3,8 +3,8 @@
 import etl, config, schema
 from loguru import logger
 
-if __name__ == "__main__":
-    logger.info("Starting worker")
+def prepare_knowledge_base():
+    logger.info("Preparing knowledge base")
     logger.info("processing transcripts")
     transcript_data, episode_data = etl.process_transcripts(
         config.transcript_dir,
@@ -15,3 +15,18 @@ if __name__ == "__main__":
         episode_data,
         config.knowledge_base_dir,
     )
+
+def prepare_search_index():
+    raise NotImplementedError("TODO")
+
+def prepare_search_index():
+    logger.info("Preparing search index")
+
+if __name__ == "__main__":
+    logger.info("Starting worker")
+
+    prepare_knowledge_base()
+    
+    prepare_search_index()
+
+
