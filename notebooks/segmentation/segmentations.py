c4d21f8e0a4d9da2b5690fcecc535beca32e3d39
Shauray <shauray9@gmail.com>
2022-07-10 15:56:35 +0530

    problems running locally

--- notebooks/segmentation/segmentations.py
+++ notebooks/segmentation/segmentations.py
@@ -1,5 +1,8 @@
 import torch
-from pyannote.audio import Pipeline
+import numpy as np
+import pandas as pd
+import argparse
+#from pyannote.audio import Pipeline
 
 ## pip install https://github.com/pyannote/pyannote-audio/archive/develop.zip
 ## git clone https://github.com/speechbrain/speechbrain.git
@@ -12,28 +15,43 @@ def diarization() -> list:
 
     return diarization
 
-def crapy_diarization() -> list:
+def crapy_diarization(audio) -> list:
     pipeline = torch.hub.load("pyannote/pyannote-audio", "dia")
-    diarization = pipeline({"audio": "audio.wav"})
+    diarization = pipeline({"audio":"H:\wav2vec_pre\audio.wav"})
 
     return diarization
 
-def get_chunks() -> list:
-    pass
+def get_chunks(trans_chunk,dizi) -> list:
+   # for _, (i,j) in enumerate(dia_list):
+   #     if start > i and start < j:
+   #         return dia_list[something]
+   return 1
 
-def main(episode:str, transcription:str) -> pd.dataframe:
+def preprocess(trans_df, dizi) -> any:
+    for row in range(len(trans_df)):
+        trans_df.iloc[row]['speaker'] = get_chunks(trans_df.iloc[row]['chunk_start'], dizi)
+    return trans_df
+
+def main(episode:str, transcription:str) -> any:
     ep = pd.read_csv(episode)
     trans = pd.read_csv(transcription)
+    trans['speaker'] = np.nan
     ep_id = list(ep.episode_id)
-    print(ep_id)
-    pass
+    print(f"ep : {ep.head()}, trans : {trans.head()}, ep_id : {ep_id}")
+    for i in ep_id:
+        dizi = crapy_diarization(ep[ep['episode_id'] == i]["file_name"])
+        print(dizi)
+        trans_df = trans[trans['episode_id'] == i]
+        trans_df = preprocess(trans_df, dizi)
+    return trans_df
 
 ## Building an ETL
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Segmentations.')
     parser.add_argument('fn_01', metavar='episode data', type=str, nargs='+',
                         help='episode data')
-    parser.add_argument('fn_02', metavar='transcription data', type=str, nargs='-',
+    parser.add_argument('fn_02', metavar='transcription data', type=str, nargs='+',
                         help='transcription data')
     args = parser.parse_args()
     segmented_transcription = main(args.fn_01[0], args.fn_02[0])
+    print(segmented_transcription)
